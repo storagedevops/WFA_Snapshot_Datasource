b9cedf2acfa8ceb5fa6653b7e787a52c286c709b
James Castro <james.castro@netapp.com>
2017-03-24 10:34:20 -0400

    Data Source Type Powershell Script
    Powershell script to place in the data source type script field

---
+++ Datasource PS GetSnapList.ps1
@@ -0,0 +1,76 @@
+﻿<#
+This script collects volume snapshot information from the Clustered Data ONTAP.
+
+Snapshot
+- Volume
+- Name
+- Timestamp
+- Cluster
+- Vserver
+
+Note : Cluster credentials must be configured in the Credentials tab.
+#>
+
+# Ensure that dates are always returned in English
+[System.Threading.Thread]::CurrentThread.CurrentCulture="en-US"
+
+# Create the output file
+$snapshot_csv = "./snapshot.csv"
+New-Item -Path $snapshot_csv -type file -force
+
+Function Get-ConnectionInfo() {
+    $connectionInfo = @{};
+
+    Try {
+        $connectionInfo["host"] = Get-WfaRestParameter "host"
+        $connectionInfo["port"] = Get-WfaRestParameter "port"
+        $connectionInfo["credentials"] = Get-WfaCredentials
+    }
+    Catch [System.Exception] {
+        $error = "Error getting data source credentials: $($_.Exception)"
+        Get-WFALogger -message $error -Error
+        Throw "Error getting data source credentials."
+    }
+
+    return $connectionInfo
+}
+$connectionInfo = Get-ConnectionInfo
+$cluster_addr = $connectionInfo["host"]
+
+Get-WFALogger -message "Retrieving list of Snapshots from Cluster $cluster_addr" -Info
+try 
+{
+  Connect-WfaCluster $cluster_addr -Timeout 300000
+}
+catch [system.exception] 
+{
+  $error = "Error connecting to cluster $cluster_addr : $($_.Exception)" 
+  Get-WFALogger -message $error -Error
+  Throw $error
+}
+
+$vservers = Get-NcVserver
+foreach ($vserver in $vservers)
+{
+    $volumes = Get-NcVol -vserver $vserver.Vserver
+    foreach($volume in $volumes)
+    {
+        $vol_snapshots = Get-NcSnapshot -vserver $vserver.Vserver -volume $volume.name
+        if ($vol_snapshots)
+        {
+            foreach($snapshot in $vol_snapshots)
+            {
+                if ($snapshot.Name -ne "")
+                {
+                    $Vserver = $snapshot.Vserver
+                    $Cluster = $cluster_addr
+                    $Volume = $snapshot.Volume
+                    $SnapshotName = $snapshot.Name
+                    $Timestamp = Get-Date ([datetime]'1/1/1970').AddSeconds($snapshot.AccessTime) -f "yyyy-MM-dd HH:mm:ss" 
+                    # Add content to file
+                    Add-Content $snapshot_csv ([byte[]][char[]] "\N`t$Cluster`t$SnapshotName`t$Timestamp`t$Volume`t$Vserver`n") -Encoding Byte
+                }
+            }
+      }
+    }
+}
