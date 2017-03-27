c92d4f959e70ec77e03c7c93ad81ec3c5bb9b324
James Castro <james.castro@netapp.com>
2017-03-27 14:40:48 -0400

    version2
    version2

--- TEST.ps1
+++ TEST.ps1
@@ -15,41 +15,39 @@ Note : Cluster credentials must be configured in the Credentials tab.
 [System.Threading.Thread]::CurrentThread.CurrentCulture="en-US"
 
 # Create the output file
-$snapshot_csv = "./snapshots.csv"
+$snapshot_csv = "c:\volume_snaps.csv"
 New-Item -Path $snapshot_csv -type file -force
 
-Function Get-ConnectionInfo() {
-    $connectionInfo = @{};
+#Function Get-ConnectionInfo() {
+    #$connectionInfo = @{};
 
-    Try {
-        $connectionInfo["host"] = Get-WfaRestParameter "host"
-        $connectionInfo["port"] = Get-WfaRestParameter "port"
-        $connectionInfo["credentials"] = Get-WfaCredentials
-    }
-    Catch [System.Exception] {
-        $error = "Error getting data source credentials: $($_.Exception)"
-        Get-WFALogger -message $error -Error
-        Throw "Error getting data source credentials."
-    }
+    #Try {
+     #   $connectionInfo["host"] = Get-WfaRestParameter "host"
+     #   $connectionInfo["port"] = Get-WfaRestParameter "port"
+      #  $connectionInfo["credentials"] = Get-WfaCredentials
+   # }
+   # Catch [System.Exception] {
+     #   $error = "Error getting data source credentials: $($_.Exception)"
+      #  Get-WFALogger -message $error -Error
+      #  Throw "Error getting data source credentials."
+    #}
 
-    return $connectionInfo
-}
-$connectionInfo = Get-ConnectionInfo
-$cluster_addr = $connectionInfo["host"]
+    #return $connectionInfo
+#}
+#$connectionInfo = Get-ConnectionInfo
+$cluster_addr = "trcl1va2cm.mhint"
 
-Get-WFALogger -message "Retrieving list of Snapshots from Cluster $cluster_addr" -Info
-try 
-{
-  Connect-WfaCluster $cluster_addr -Timeout 300000
-}
-catch [system.exception] 
-{
-  $error = "Error connecting to cluster $cluster_addr : $($_.Exception)" 
-  Get-WFALogger -message $error -Error
-  Throw $error
-}
-
-$ClusterName = Get-NcCluster | select ClusterName
+#Get-WFALogger -message "Retrieving list of Snapshots from Cluster $cluster_addr" -Info
+#try 
+#{
+ # Connect-WfaCluster $cluster_addr -Timeout 300000
+#}
+#catch [system.exception] 
+#{
+  #$error = "Error connecting to cluster $cluster_addr : $($_.Exception)" 
+ # Get-WFALogger -message $error -Error
+  #Throw $error
+#}
 
 $vservers = Get-NcVserver
 foreach ($vserver in $vservers)
@@ -62,17 +60,16 @@ foreach ($vserver in $vservers)
         {
             foreach($snapshot in $vol_snapshots)
             {
-                if ($snapshot.Name -notlike "snapmirror*")
+                if ($snapshot.Name -ne "")
                 {
                     $Vserver = $snapshot.Vserver
-                    $Cluster = $ClusterName.ClusterName
+                    $Cluster = $cluster_addr
                     $Volume = $snapshot.Volume
                     $SnapshotName = $snapshot.Name
                     $Dependency = $snapshot.Dependency
-                    $SnapshotID = $snapshot.SnapshotInstanceUuid
                     $Timestamp = Get-Date ([datetime]'1/1/1970').AddSeconds($snapshot.AccessTime) -f "yyyy-MM-dd HH:mm:ss" 
                     # Add content to file
-                    Add-Content $snapshot_csv ([byte[]][char[]] "\N`t$Cluster`t$SnapshotName`t$Timestamp`t$Volume`t$Vserver`t$Dependency`t$SnapshotID`n") -Encoding Byte
+                    Add-Content $snapshot_csv ([byte[]][char[]] "\N`t$Cluster`t$SnapshotName`t$Timestamp`t$Volume`t$Vserver`t$Dependency`n") -Encoding Byte
                     # This is required to ensure that the output file is UNIX encoded, without which MySQL's LOAD DATA
 					# command does not work
                 }
