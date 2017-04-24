7d5ca5f5daf878cf12e251636de0ee7b7cf66edf
James Castro <james.castro@netapp.com>
2017-04-24 12:13:41 -0400

    Fixed $Timestamp variable that was adding 3 extra hours

--- finalDSsnapworkingversion.ps1
+++ finalDSsnapworkingversion.ps1
@@ -220,7 +220,7 @@ $vol_snapshots = Get-NcSnapshot -Query $query -Attributes $attr
                     $SnapshotName = $snapshot.Name
                     $Dependency = $snapshot.Dependency
                     $SnapshotID = $snapshot.SnapshotInstanceUuid
-                    $Timestamp = Get-Date ([datetime]'1/1/1970').AddSeconds($snapshot.AccessTime) -f "yyyy-MM-dd HH:mm:ss" 
+                    $Timestamp = ($snapshot.AccessTimeDT).ToString("yyyy-MM-dd HH:mm:ss") 
                     # Add content to file
                     Add-Content $snapshot_csv ([byte[]][char[]] "\N`t$Cluster`t$SnapshotName`t$Timestamp`t$Volume`t$Vserver`t$Dependency`t$SnapshotID`n") -Encoding Byte
                     # This is required to ensure that the output file is UNIX encoded, without which MySQL's LOAD DATA
